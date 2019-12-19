930e77956dfea5bd8c0ab49f798f5f70bef8198c
Artem Bespolov <abespolov@gmail.com>
2019-12-19 13:02:55 +0300

    fix/update-keys

---
+++ index.js
@@ -0,0 +1,75 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const csrf = require('csurf');
+const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongodb-session')(session);
+const flash = require('connect-flash');
+const varMiddleware = require('./middleware/variables');
+const userMiddleware = require('./middleware/user');
+const keys = require('./keys');
+
+const homeRoutes = require('./routes/home');
+const coursesRoutes = require('./routes/courses');
+const addRoutes = require('./routes/add');
+const cardRoutes = require('./routes/card');
+const ordersRoutes = require('./routes/orders');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+});
+
+const store = new MongoStore({
+    collection: 'session',
+    uri: keys.MONGODB_URI
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: true}));
+app.use(session({
+    secret: keys.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store
+}));
+
+app.use(csrf());
+app.use(flash());
+app.use(varMiddleware);
+app.use(userMiddleware);
+
+
+app.use('/', homeRoutes);
+app.use('/courses', coursesRoutes);
+app.use('/add', addRoutes);
+app.use('/card', cardRoutes);
+app.use('/orders', ordersRoutes);
+app.use('/auth', authRoutes);
+
+async function start() {
+    const port = process.env.PORT || 3000;
+
+    try {
+        await mongoose.connect(keys.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        });
+        app.listen(port, () => {
+            console.log('Server listening at port ' + port);
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start();
